037f1102ec7c6d866954184339ea02a31755a8a9
Ayman Haque <aymanhaque001@gmail.com>
2024-01-16 21:26:54 -0600

    Fixed yaml

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+# from airflow.operators.python import PythonOperator
 import uuid
 import json
 import requests
+from kafka import KafkaProducer
 
-
-default_args ={
+default_args = {
     'owner': 'Ayman',
-    'start_date': datetime(2024,16,1, 10, 00)
+    'start_date': datetime(2024, 1, 16, 10, 00)
 }
 
 
@@ -18,10 +18,11 @@ def get_data():
     res = res['results'][0]
     return res
 
+
 def format_data(res):
     data = {}
     location = res['location']
-    data['id'] = uuid.uuid4()
+    data['id'] = str(uuid.uuid4())
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
@@ -40,15 +41,21 @@ def format_data(res):
 
 def stream_data():
     data = get_data()
-    data= format_data(data)
+    data = format_data(data)
+    print(data)
+    producer = KafkaProducer(
+        bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    producer.send('users_created', json.dumps(data).encode('utf-8'))
 
 
+# with DAG('user_automation',
+#          default_args=default_args,
+#          schedule_interval='@daily',
+#          catchup=False) as dag:
+#     streaming_task = PythonOperator(
+#         task_id='stream_data_from_api',
+#         python_callable=stream_data
+#     )
 
-with DAG('user_automation',
-        default_args= default_args,
-        schedule_interval='@daily',
-        catchup = False) as dag:
-        streaming_task = PythonOperator(
-    task_id= 'stream_data_from_api',
-    python_callable= stream_data
-    )
+
+stream_data()
